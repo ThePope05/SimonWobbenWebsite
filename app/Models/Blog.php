b06720bb3d2e5d6f114132002c6df881c5838ae6
ThePope05 <swobben05@gmail.com>
2024-03-20 23:59:01 +0100

    Add Blog, Comment, and Favorite models

---
+++ app/Models/Blog.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'content', 'likes', 'favorites'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+}
