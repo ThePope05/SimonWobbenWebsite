751bde47d57bfc2e51ace541684ebf49bf94c60f
ThePope05 <swobben05@gmail.com>
2024-03-20 23:59:49 +0100

    Add new middleware and components, update routes

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,16 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+//Middlewares
+use App\Http\Middleware\Authenticate;
+
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(Authenticate::class)->name('dashboard');
 
 Route::get('/blogs', function () {
     return view('blogs.index');
@@ -19,6 +22,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::group([], function () {
+    Route::get('/general/no_access_page', function () {
+        return view('general.pleaseLogin');
+    })->name('general.no_access_page');
 });
 
 require __DIR__ . '/auth.php';
