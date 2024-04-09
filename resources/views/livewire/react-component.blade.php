097de07df305e0fa70b8fd1101eb8903755a352e
ThePope05 <swobben05@gmail.com>
2024-04-09 21:31:03 +0200

    Added transition to react component

--- resources/views/livewire/react-component.blade.php
+++ resources/views/livewire/react-component.blade.php
@@ -1,21 +1,21 @@
 <div class="text-gray-900 w-2/4 flex flex-row">
     <button wire:click="checkReact('favorite')" class="{{ $this->isReactType('favorite') ? 'text-pink-700' : '' }}">
-        <span class="material-symbols-outlined">
+        <span class="material-symbols-outlined transition-colors ">
             favorite
         </span>
     </button>
     <button wire:click="checkReact('mood')" class="{{ $this->isReactType('mood') ? 'text-pink-700' : '' }}">
-        <span class="material-symbols-outlined">
+        <span class="material-symbols-outlined transition-colors ">
             mood
         </span>
     </button>
     <button wire:click="checkReact('sentiment_satisfied')" class="{{ $this->isReactType('sentiment_satisfied') ? 'text-pink-700' : '' }}">
-        <span class="material-symbols-outlined">
+        <span class="material-symbols-outlined transition-colors ">
             sentiment_satisfied
         </span>
     </button>
     <button wire:click="checkReact('sentiment_dissatisfied')" class="{{ $this->isReactType('sentiment_dissatisfied') ? 'text-pink-700' : '' }}">
-        <span class="material-symbols-outlined">
+        <span class="material-symbols-outlined transition-colors ">
             sentiment_dissatisfied
         </span>
     </button>
